414cfabe91c1a0df1795e6e20b3e9145d23cbf82
HOVOH <71901758+HOVOH@users.noreply.github.com>
2021-06-03 11:44:35 -0400

    add status error map

--- src/error.codes.ts
+++ src/error.codes.ts
@@ -1,5 +1,13 @@
+import { HttpStatus } from "@nestjs/common";
+
 export const BAD_REFRESH_TOKEN = 'bad_refresh_token';
 export const BAD_ACCESS_TOKEN = 'bad_access_token';
 export const BAD_CREDENTIALS = 'bad_credentials';
 export const NO_ACCESS_TOKEN = 'no_access_token';
 export const NO_REFRESH_TOKEN = 'no_refresh_token';
+
+export const authErrorStatusMap = {
+  [NO_ACCESS_TOKEN]: HttpStatus.UNAUTHORIZED,
+  [NO_REFRESH_TOKEN]: HttpStatus.UNAUTHORIZED,
+  [BAD_ACCESS_TOKEN]: HttpStatus.FORBIDDEN,
+}
