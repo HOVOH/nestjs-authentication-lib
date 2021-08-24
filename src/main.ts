76f47d71de83d2ff6803594554c6c2c95b4deadb
HOVOH <71901758+HOVOH@users.noreply.github.com>
2021-08-23 23:56:59 -0400

    remove test helper export from main

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import { AccessTokenGuard } from './access-token/access-token.guard';
 import { ACCESS_COOKIE_NAME } from './access-token/constants';
 import { ReqSession } from "./req-session.decorator";
 import {Public, isPublic} from "./access-token/public.decorator";
-import {FakeAccessTokenGuard, iAccessTokenFactory } from "./testhelpers";
-
-"./testhelpers"
 
 export {
   ACCESS_COOKIE_NAME,
@@ -19,6 +16,4 @@ export {
   ReqSession,
   SignedAccessToken,
   Session,
-  FakeAccessTokenGuard,
-  iAccessTokenFactory
 };
